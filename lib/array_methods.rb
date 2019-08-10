568b0564196d1d0dae66e3dfb703bd716a0a3a5d
Martin Milius <martin.milius@gmail.com>
2019-08-10 16:13:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -7,11 +7,10 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  max = 0 
   length = array.length
   array.length.times do []
 end
-return 
+return array.
 end
 
 def find_min_value(array)
