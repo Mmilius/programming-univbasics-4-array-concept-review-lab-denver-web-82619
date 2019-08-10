181330c873ef33ae8b8d26af151b9a22a73a78d4
Martin Milius <martin.milius@gmail.com>
2019-08-10 15:56:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
  length = array.length
- array.length.times do [index]
+ array.length.times do [value_to_find]
  puts array []
  end
  return array.index(value_to_find)
